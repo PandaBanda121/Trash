13503b1c418c56b9104eeef14c2b6fe0ebe75137
PandaBanda121 <panda121banda121@gmail.com>
2025-02-03 16:46:42 +0800

    init

---
+++ game.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <iomanip>
+#include <unistd.h>
+#include <sstream>
+
+
+#include <Windows.h>
+#pragma execution_character_set("utf-8")
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
